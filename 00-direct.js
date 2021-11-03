4f95ac5ff99c6d8e46be3f533dc80d78eadfd553
Adron Hall <adron@thrashingcode.com>
2021-11-02 20:34:28 -0700

    Missed the code in that last one.

---
+++ 00-direct.js
@@ -0,0 +1,16 @@
+// The following is a minimal direct recursion call function.
+var countdownDisplayString = ""
+
+function Countdown(theCount, countdown)
+{
+    if (0 < theCount)
+    {
+        countdown = countdown + theCount + " "
+        theCount = theCount - 1
+        return Countdown(theCount, countdown)
+    }
+    return countdown.trim()
+}
+
+console.log("Result of direct recursion function.")
+console.log(Countdown(3, countdownDisplayString))
--- 01-indirect.js
+++ 01-indirect.js
@@ -1,17 +1,53 @@
-// The following is a minimal direct recursion call function.
-var countdownDisplayString = ""
-
-function Countdown(theCount, countdown)
-{
-    if (0 < theCount)
-    {
-        countdown = countdown + theCount + " "
-        theCount = theCount - 1
-        return Countdown(theCount, countdown)
-    }
-    return countdown.trim()
+var foodSelections ={
+    BerriesAndThings: ['orange','peach','tomato','potato','kiwi','strawberry','ground beer','blackberry','cherry','boysenberry'],
+    Meats: {
+        Beef: ['Filet Mignon', 'Hamburger Steak', 'Ribeye', 'Sirloin'],
+        Chicken: ['Chicken Marsala', 'Fried Chicken', 'Nuggets'],
+        Seafood: {
+            Saltwater: ['Tuna','Swordfish','Salmon','Hake'],
+            Freshwater: ['Bluegill','Crappie','Catfish','Trout','White Bass','Walleye'],
+        },
+    },
+    Sweets: ['Bueno','Snickers','Milkyway'],
 }
 
-console.log("Result of direct recursion function.")
-console.log(Countdown(3, countdownDisplayString))
+var foodSelectionTwo = ['beef','filet','steak']
 
+function FindFood(food) {
+    var foundFood = ByName(food)
+    if (foundFood != null) {
+        return 'Found your food!\n' + foundFood
+    }
+    if (foundFood == null) {
+        foundFood = ByPart(food)
+        return 'This might be your food.\n' + foundFood
+    }
+    return 'No food found, try again!'
+}
+
+function ByName(food) {
+
+}
+
+function ByPart(food) {
+
+}
+
+// function scan(obj) {
+//     var k;
+//     if (obj instanceof Object) {
+//         for (k in obj){
+//             if (obj.hasOwnProperty(k)){
+//                 //recursive call to scan property
+//                 scan( obj[k] );
+//             }
+//         }
+//     } else {
+//         //obj is not an instance of Object so obj here is a value
+//     };
+//
+// };
+
+console.log('Finding some food!')
+console.log(FindFood('peach'))
+console.log(FindFood('berry'))
